7663bddb925848d74cecbb90ecf64ef2e1dbca73
Giovanne Afonso <giovanneafonso@gmail.com>
2021-01-29 15:43:39 -0300

    Add videos to react-basico

--- falcor/models/trainings/react-basico.js
+++ falcor/models/trainings/react-basico.js
@@ -1,26 +1,119 @@
 import falcor from "falcor";
 
 export default function (root) {
+  const topic = (topicSlug) => ({
+    $type: "ref",
+    value: [...root, "topics", "by-slug", topicSlug],
+  });
+
   return {
     slug: root[root.length - 1],
     title: "React Básico",
     description: "Curso de React Básico",
     topics: {
       asc: [
-        {
-          $type: "ref",
-          value: [...root, "topics", "by-slug", "separacao-de-componentes"],
-        },
-        {
-          $type: "ref",
-          value: [...root, "topics", "by-slug", "separacao-de-componentes"],
-        },
-        {
-          $type: "ref",
-          value: [...root, "topics", "by-slug", "separacao-de-componentes"],
-        },
+        topic("curiosidades-aleatorias-sobre-react"),
+        topic("hello-world-react"),
+        topic("entendendo-sobre-componentes-react"),
+        topic("react-classes-vs-hooks"),
+        topic("react-props-state"),
+        topic("comunicacao-componentes-react"),
       ],
       "by-slug": {
+        // "": {
+        //   title: "",
+        //   description: "",
+        //   videos: [],
+        //   articles: [],
+        // },
+        "curiosidades-aleatorias-sobre-react": {
+          title: "Curiosidades sobre React",
+          description:
+            "Vídeos que vão te deixar por dentro do Mundo React. Sem tema específico, apenas conteúdo interessante sobre React.",
+          videos: [
+            {
+              url: "https://www.youtube.com/watch?v=DTy3oIU-NN4",
+              title:
+                "React vs Vue vs Angular - Qual framework frontend escolher para 2021?",
+              description: `
+                - Qual tem o melhor mercado de trabalho?
+                - Qual mais fácil de aprender?
+                - Qual tem a melhor comunidade?
+                - Qual resolve mais problemas?
+              `,
+            },
+            {
+              url: "https://www.youtube.com/watch?v=S-trXOddXMQ",
+              title:
+                "Como Saber Se Você Já É Um Programador Pleno? // Vlog #147",
+              description:
+                "Afinal de contas quando um desenvolvedor deixa de ser Junior e se torna um Desenvolvedor Pleno?",
+            },
+            {
+              url: "https://www.youtube.com/watch?v=2a8F5QZEZ1c",
+              title:
+                "Minha evolução de salário como PROGRAMADOR - Quanto eu ganho HOJE?",
+              description:
+                "O vídeo de hoje fala um pouco sobre quanto ganho hoje e como foi minha evolução de salário até agora, falando abertamente sobre valores e tudo mais.",
+            },
+            // {
+            //   url: '',
+            //   title: '',
+            //   description: '',
+            // }
+            // {
+            //   url: '',
+            //   title: '',
+            //   description: '',
+            // }
+          ],
+          articles: [],
+        },
+        "hello-world-react": {
+          title: "Hello World",
+          description: "Aprenda a criar um projeto react e começar a programar",
+          videos: [
+            {
+              url: "https://www.youtube.com/watch?v=LqP7ks7rAEY",
+              title: "ReactJS - Criando primeiro projeto",
+              description:
+                "Nesse video damos os primeiros passos com ReactJS criando o seu primeiro projeto na pratica e entendendo passo a passo.",
+            },
+            {
+              url: "https://www.youtube.com/watch?v=s9Whqnmh-a8",
+              title: "Entendendo os Arquivos e Estrutura do Projeto",
+              description:
+                "Vídeo com o objetivo de explicar a organização dos arquivos e pastas do projeto que você criou seguindo o vídeo anterior.",
+            },
+          ],
+          articles: [],
+        },
+        "entendendo-sobre-componentes-react": {
+          title: "Entendendo Sobre Componentes",
+          description: "",
+          videos: [],
+          articles: [],
+        },
+        "react-classes-vs-hooks": {
+          title: "Classes vs Hooks",
+          description:
+            "Entenda a diferença entre componentes de classe e componentes funcionais (que são uma função ao invés de uma classe)",
+          videos: [],
+          articles: [],
+        },
+        "react-props-state": {
+          title: "Props e State",
+          description: "Aprenda sobre Props e States de um componente",
+          videos: [],
+          articles: [],
+        },
+        "comunicacao-componentes-react": {
+          title: "Comunicação entre Componentes",
+          description:
+            "Aprenda a passar dados entre os seus componentes React!",
+          videos: [],
+          articles: [],
+        },
         "separacao-de-componentes": {
           title: "Separação de Componentes",
           description:
