06f1285c70ee57c0eb35031969dfc04eaba527d4
Micaella Danna <the.daanm@gmail.com>
2021-02-01 18:39:41 -0300

    adc comentarios no arquivo

--- components/BannerQuizNivelamento/styles.js
+++ components/BannerQuizNivelamento/styles.js
@@ -1,10 +1,12 @@
-import styled from "styled-components";
+import styled from "styled-components"; //importa styles para os componentes
 
+//adc esse style ao container Wrapper, do index.js
 export const Wrapper = styled.div`
   padding: 100px 0;
   background: #222266;
 `;
 
+//adc esse style na parte <S.TesteNivelamento></S.TesteNivelamento> do index.js
 export const TesteNivelamento = styled.p`
   color: #98b;
   margin-bottom: 0;
@@ -12,11 +14,13 @@ export const TesteNivelamento = styled.p`
   font-size: 18px;
 `;
 
+//adc esse style na parte <S.Title></S.Teitle> do index.js
 export const Title = styled.h3`
   font-size: 28px;
   color: #eee;
 `;
 
+//adc esse style na parte <S.Description></S.Description> do index.js
 export const Description = styled.p`
   margin-bottom: 30px;
   font-size: 20px;
