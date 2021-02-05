0f1f838e705374f931001166f5bf434f7cff218a
Micaella Danna <the.daanm@gmail.com>
2021-02-04 22:45:58 -0300

    banner organizado para screen max-width 600px

--- components/BannerQuizNivelamento/styles.js
+++ components/BannerQuizNivelamento/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"; //importa styles para os componentes
 
 //adc esse style ao container Wrapper, do index.js
 export const Wrapper = styled.div`
-  padding: 100px 0;
+  padding: 50px 0;
   background: #222266;
 `;
 
@@ -16,14 +16,14 @@ export const TesteNivelamento = styled.p`
 
 //adc esse style na parte <S.Title></S.Teitle> do index.js
 export const Title = styled.h3`
-  font-size: 28px;
+  font-size: 22px;
   color: #eee;
 `;
 
 //adc esse style na parte <S.Description></S.Description> do index.js
 export const Description = styled.p`
   margin-bottom: 30px;
-  font-size: 20px;
+  font-size: 18px;
   color: #77a;
   max-width: 500px;
 `;
